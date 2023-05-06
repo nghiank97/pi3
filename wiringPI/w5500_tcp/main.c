6e142f1df2f1d985562e96f06e2bed58bd5ac830
nghiank97 <nghia.nk97@gmail.com>
2023-05-07 00:33:29 +0700

    w5500 tcp

---
+++ wiringPI/w5500_tcp/main.c
@@ -0,0 +1,135 @@
+
+#include "main.h"
+#include "w5500.h"
+#include "socket.h"
+#include "wizchip_conf.h"
+
+#define spi_chanel 	  0
+#define spi_speed 	  500000
+#define	spi_cs_pin		17
+#define	w5500_rst_pin	14
+int spi_fd;
+
+int w5500_spi_init(void){
+  /* setup gpio */
+  wiringPiSetupGpio();
+	pinMode(spi_cs_pin, OUTPUT);
+	digitalWrite(spi_cs_pin, 1);
+	pinMode(w5500_rst_pin, OUTPUT);
+	digitalWrite(w5500_rst_pin, 1);
+  /* setup spi */
+	if( (wiringPiSPISetup(spi_chanel, spi_speed)) <0){
+		printf("SPI Setup failed\n");
+		return 0;
+	}
+ 
+	spi_fd = wiringPiSPIGetFd(0);
+  return 1;
+}
+
+void w5500_reset(void){
+	digitalWrite(w5500_rst_pin, 0);
+	delay(1);
+	digitalWrite(w5500_rst_pin, 1);
+	delay(1000);
+}
+void w5500_cs_l(void){
+	digitalWrite(spi_cs_pin, 0);
+}
+void w5500_cs_h(void){
+	digitalWrite(spi_cs_pin, 1);
+}
+void w5500_spi_write(u08 data){
+	write(spi_fd,&data, 1);
+}
+u08 w5500_spi_read(void){
+	u08 c = 0;
+	read(spi_fd,&c, 1);
+	return c;
+}
+void w5500_spi_write_page(u08* pBuf, u16 len){
+	write(spi_fd,pBuf, len);
+}
+void w5500_spi_read_page(u08* pBuf, u16 len){
+	read(spi_fd,pBuf, len);
+}
+
+u08 txsize[8] = {4,2,2,2,2,2,2,2};
+u08 rxsize[8] = {4,2,2,2,2,2,2,2};
+
+wiz_NetInfo gWIZNETINFO = {
+							.mac = {0x08,0x10,0x19,0x97,0x25,0x25},
+							.ip = {192,168,7,47},
+							.sn = {255, 255, 255, 0},
+							.gw = {192, 168, 7, 1},
+							.dns = {8, 8, 8, 8},
+							.dhcp = NETINFO_STATIC
+};
+#define W5500_DEBUG
+#define ETH_MAX_BUF_SIZE	2048
+u08 buffer[ETH_MAX_BUF_SIZE];
+
+/* udp info */
+u08 server_ip[4]={192,168,7,100};
+u16 server_port=5002;
+u16 local_port=5004;
+u16 len=0;
+
+extern void setup(){
+	w5500_spi_init();
+	/* initialize w5500 */
+	w5500_reset();
+	reg_wizchip_cs_cbfunc(w5500_cs_l, w5500_cs_h);
+	reg_wizchip_spi_cbfunc(w5500_spi_read, w5500_spi_write);
+	reg_wizchip_spiburst_cbfunc(w5500_spi_read_page, w5500_spi_write_page);
+	wizchip_init(txsize, rxsize);
+	wizchip_setnetinfo(&gWIZNETINFO);
+#ifdef W5500_DEBUG
+	u08 net_ip[4] = {0};
+	u08 net_sub[4] = {0};
+	u08 net_gw[4] = {0};
+	u08 net_mac[6] = {0};
+	
+	getSIPR(net_ip);
+	getSHAR(net_mac);
+	getSUBR(net_sub);
+	getGAR(net_gw);
+	
+	printf("ip: %03d.%03d.%03d.%03d\r\n", net_ip[0], net_ip[1], net_ip[2], net_ip[3]);
+	printf("sn: %03d.%03d.%03d.%03d\r\n", net_sub[0], net_sub[1], net_sub[2], net_sub[3]);
+	printf("gw: %03d.%03d.%03d.%03d\r\n", net_gw[0], net_gw[1], net_gw[2], net_gw[3]);
+	printf("mac: %02x.%02x.%02x.%02x.%02x.%02x\r\n", net_mac[0], net_mac[1], net_mac[2], net_mac[3], net_mac[4], net_mac[5]);
+#endif
+}
+
+extern void loop(){
+	switch(getSn_SR(0)){
+		case SOCK_INIT:
+			connect(0, server_ip,server_port);
+			break;
+		case SOCK_ESTABLISHED:
+			if(getSn_IR(0) & Sn_IR_CON){
+				setSn_IR(0, Sn_IR_CON);
+			}
+			len=getSn_RX_RSR(0);
+			if(len>0){
+				recv(0,buffer,len);
+				printf("%s\r\n",buffer);
+				send(0,buffer,len);
+			}
+			break;
+		case SOCK_CLOSE_WAIT:
+			_close(0);
+			break;
+		case SOCK_CLOSED:
+			socket(0,Sn_MR_TCP,local_port,Sn_MR_ND);
+		break;
+	}
+}
+
+int main(){
+  setup();
+  while(1){
+    loop();
+  }
+}
--- wiringPI/w5500_udp/CMakeFiles/main.c.dir/cmake_clean_target.cmake
+++
@@ -1,3 +0,0 @@
-file(REMOVE_RECURSE
-  "libmain.c.a"
-)
