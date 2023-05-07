5058ce14a71326e912f15f1ce38cfd90ef80d9d4
Khac Nghia <nghia.nk97@gmail.com>
2023-05-07 23:20:19 +0700

    udp polling

--- wiringPI/w5500_udp/main.c
+++ wiringPI/w5500_udp/main.c
@@ -2,92 +2,135 @@
 #include "main.h"
 #include "w5500.h"
 #include "socket.h"
-u08 mac[6]={0x08,0x10,0x19,0x97,0x25,0x25};   /* mac */
-u08 ip[4]={192,168,7,100};                   	/* ip */
-u08 sub[4]={255,255,255,0};                   /* subnet */
-u08 gw[4]={192,168,7,1};                     	/* gateway */
+#include "wizchip_conf.h"
 
-u08 txsize[8] = {2,2,2,2,2,2,2,2};
-u08 rxsize[8] = {2,2,2,2,2,2,2,2};
+#define spi_chanel 	  	0
+#define spi_speed 	  	32000000
+#define	spi_cs_pin	17
+#define	w5500_rst_pin	14
+#define	w5500_int_pin	15
+int spi_fd;
 
-u08 _remote_ip[4] = {0};
-u08 _port[2] = {0};
-u08 _remote_port[2] = {0};
-	
+int w5500_spi_init(void){
+  /* setup gpio cs, rst */
+  wiringPiSetupGpio();
+  pinMode(spi_cs_pin, OUTPUT);
+  digitalWrite(spi_cs_pin, 1);
+  pinMode(w5500_rst_pin, OUTPUT);
+  digitalWrite(w5500_rst_pin, 1);
+   
+  /* set Pin 15 generate an interrupt on high-to-low transitions
+  if ( wiringPiISR (w5500_int_pin, INT_EDGE_FALLING, &w5500_hw_int_cp) < 0 ) {
+      printf ("Unable to setup ISR: %s\n");
+      return 1;
+  }
+   */
+
+  /* setup spi */
+  if( (wiringPiSPISetup(spi_chanel, spi_speed)) <0){
+	printf("SPI Setup failed\n");
+	return 0;
+  }
+  spi_fd = wiringPiSPIGetFd(0);
+  return 1;
+}
+
+void w5500_reset(void){
+	digitalWrite(w5500_rst_pin, 0);
+	delay(1);
+	digitalWrite(w5500_rst_pin, 1);
+	delay(1000);
+}
+void w5500_cs_l(void){
+	digitalWrite(spi_cs_pin, 0);
+}
+void w5500_cs_h(void){
+	digitalWrite(spi_cs_pin, 1);
+}
+void w5500_spi_write(u08 data){
+	write(spi_fd,&data, 1);
+}
+u08 w5500_spi_read(void){
+	u08 c = 0;
+	read(spi_fd,&c, 1);
+	return c;
+}
+void w5500_spi_write_page(u08* pBuf, u16 len){
+	write(spi_fd,pBuf, len);
+}
+void w5500_spi_read_page(u08* pBuf, u16 len){
+	read(spi_fd,pBuf, len);
+}
+
+u08 txsize[8] = {4,2,2,2,2,2,2,2};
+u08 rxsize[8] = {4,2,2,2,2,2,2,2};
+
+wiz_NetInfo gWIZNETINFO = {
+							.mac = {0x08,0x10,0x19,0x97,0x25,0x25},
+							.ip = {192,168,7,47},
+							.sn = {255, 255, 255, 0},
+							.gw = {192, 168, 7, 1},
+							.dns = {8, 8, 8, 8},
+							.dhcp = NETINFO_STATIC
+};
+#define W5500_DEBUG
+#define ETH_MAX_BUF_SIZE	2048
+u08 buffer[ETH_MAX_BUF_SIZE];
 
 /* udp info */
-SOCKET_STATUS status_socket = SOCK_CLOSED;
-u08 dummy = 0;
-u08 port[2] = {0};
-u16 local_port = 5002;
-u16 remote_port = 5004;
-u08 remote_ip[4]={192,168,7,10};
+u08 remote_ip[4]={192,168,7,100};
+u16 remote_port=5002;
+u16 local_port=5004;
+u16 len=0;
 
 extern void setup(){
-	w5500_init(mac, ip, sub, gw);
-	w5500_set_size(txsize, rxsize);
-	
-	w5500_retry_time_value(2000);
-	w5500_retry_count(3);
-	/* setup udp */
-  socket_close(0);
-	/* 1. configures the protocol mode of socket 0 */
-	dummy = Sn_MR_PROTOCOL_UDP;
-	w5500_write_access(Sn_MR, SOCKET_0_REGISTER, &dummy, 1);delay(1);
-	// write local port
-	port[0] = (u08)(local_port>> 8);
-	port[1] = (u08)(local_port & 0x00ff);
-	w5500_write_access(Sn_PORT ,SOCKET_0_REGISTER, port, 2);delay(1);
-	// write destination ip address port
-  w5500_write_access(Sn_DIPR ,SOCKET_0_REGISTER, remote_ip, 4);delay(1);
-	// write destination port
-	port[0] = (u08)(remote_port >> 8);
-	port[1] = (u08)(remote_port & 0x00ff);
-	w5500_write_access(Sn_DPORT ,SOCKET_0_REGISTER, port, 2);delay(1);
-	
+	w5500_spi_init();
+	/* initialize w5500 */
+	w5500_reset();
+	reg_wizchip_cs_cbfunc(w5500_cs_l, w5500_cs_h);
+	reg_wizchip_spi_cbfunc(w5500_spi_read, w5500_spi_write);
+	reg_wizchip_spiburst_cbfunc(w5500_spi_read_page, w5500_spi_write_page);
+	wizchip_init(txsize, rxsize);
+	wizchip_setnetinfo(&gWIZNETINFO);
 #ifdef W5500_DEBUG
-	w5500_read_access(Sn_PORT ,SOCKET_0_REGISTER, _port, 2);delay(1);
-	w5500_read_access(Sn_DIPR ,SOCKET_0_REGISTER, _remote_ip, 4);delay(1);
-	w5500_read_access(Sn_DPORT ,SOCKET_0_REGISTER, _remote_port, 2);delay(1);
+	u08 net_ip[4] = {0};
+	u08 net_sub[4] = {0};
+	u08 net_gw[4] = {0};
+	u08 net_mac[6] = {0};
 	
+	getSIPR(net_ip);
+	getSHAR(net_mac);
+	getSUBR(net_sub);
+	getGAR(net_gw);
 	
-	printf("\r\nUDP INFO............\r\n");
-	printf("port: %04d\r\n", _port[0]*256 + _port[1]);
-	printf("remote ip: %03d.%03d.%03d.%03d\r\n", _remote_ip[0], _remote_ip[1], _remote_ip[2], _remote_ip[3]);
-	printf("remote port: %04d\r\n", _remote_port[0]*256 + _remote_port[1]);
+	printf("ip: %03d.%03d.%03d.%03d\r\n", net_ip[0], net_ip[1], net_ip[2], net_ip[3]);
+	printf("sn: %03d.%03d.%03d.%03d\r\n", net_sub[0], net_sub[1], net_sub[2], net_sub[3]);
+	printf("gw: %03d.%03d.%03d.%03d\r\n", net_gw[0], net_gw[1], net_gw[2], net_gw[3]);
+	printf("mac: %02x.%02x.%02x.%02x.%02x.%02x\r\n", net_mac[0], net_mac[1], net_mac[2], net_mac[3], net_mac[4], net_mac[5]);
 #endif
-	
-	// opened
-	dummy = Sn_CR_OPEN;
-	w5500_write_access(Sn_CR ,SOCKET_0_REGISTER, &dummy, 1);delay(1);
-	// check Sn_CR
-	while(1){
-		dummy = 0;
-		w5500_read_access(Sn_CR ,SOCKET_0_REGISTER, &dummy, 1);delay(1);
-		if (dummy == 0x01){
-			#ifdef W5500_DEBUG
-				printf("Socket opened \r\n");
-			#endif
-			break;
-		}
-	}
 }
 
 extern void loop(){
-	status_socket = w5500_sn_sr(0);
-	switch (status_socket){
-		case SOCK_CLOSED:{
-		}
-		case SOCK_UDP:{
+	u08 s = getSn_SR(0);
+	switch(s){
+		case SOCK_UDP:
+	      		len=getSn_RX_RSR(0);
+			if(getSn_IR(0) & Sn_IR_RECV){
+				setSn_IR(0, Sn_IR_RECV);
+			}
+			if(len>0){
+				memset(buffer,0,len+1);
+				recvfrom(0,buffer, len, remote_ip,&remote_port);
+				sendto(0,buffer,len, remote_ip, remote_port);
+			}
 			break;
-		}
+		case SOCK_CLOSED:
+			socket(0,Sn_MR_UDP,local_port,0);
+			break;
 		default:
 			break;
 	}
-#ifdef W5500_DEBUG
-	printf("\r\nSOCKET STATUS : %02x\r\n",status_socket);
-#endif
-	delay(100);
+  	delay(50);
 }
 
 int main(){
