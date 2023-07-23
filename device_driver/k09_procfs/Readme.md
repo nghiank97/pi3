87f814c0c7e457e8d47991b3dce23fb7f63f51fb
knghia <nghia.nk97@gmail.com>
2023-07-23 22:38:12 +0700

    procfs

--- device_driver/k09_procfs/Readme.md
+++ device_driver/k09_procfs/Readme.md
@@ -1,82 +1,18 @@
-# Create the file operation
-```
-    cdev_init(&my_cdev, &fops);
-    my_cdev.owner = THIS_MODULE;
-    my_cdev.ops = &fops;
-```
-# Create the thread
-```
-    wait_thread = kthread_create(wait_function, NULL, "WaitThread");
-    if (wait_thread) {
-      pr_info(" Thread Created successfully\n");
-      wake_up_process(wait_thread);
-    } else
-      pr_info(" Thread creation failed\n");
-```
-# Create the wait function
-```
-static int wait_function(void *unused){
-  while(1) {
-    pr_info(" Waiting For Event...\n");
-    wait_event_interruptible(wait_queue_etx, wait_queue_flag != 0 );
-    switch (wait_queue_flag){
-      case 1:{
-        pr_info(" Event Came From Read Function : %d\n", ++read_count);
-        wait_queue_flag = 0;
-        break;
-      }
-      case 2:{
-        pr_info(" Event Came From Write Function : %d\n", ++read_count);
-        wait_queue_flag = 0;
-        break;
-      }
-      case 3:{
-        pr_info(" Event Came From Exit Function\n");
-        return 0;
-      }
-    }
-  }
-  do_exit(0);
-  return 0;
-}
-```
-# Start the device driver
-```
-	$ make start
-```
-```
-[ 6017.166164]  Starting
-[ 6017.166186]  Major = 239 Minor = 0
-[ 6017.167554]  Thread Created successfully
-[ 6017.167573]  Device Driver Insert...Done!!!
-[ 6017.167580]  Waiting For Event...
-```
-pr_info(" Waiting For Event...\n");
-wait_event_interruptible(wait_queue_etx, wait_queue_flag != 0 );
--> "[ 6017.167580]  Waiting For Event..."
 
-# Write the device file
-```
-	$ sudo echo 1 >/dev/my_dev
-```
-```
-[ 6254.263289]  Drive open function called
-[ 6254.311288]  Write Function
-[ 6254.311335]  Drive write function call
-[ 6254.311361]  Event Came From Write Function : 1
-[ 6254.311374]  Waiting For Event...
-[ 6254.316629]  Drive release function call
-```
-```
-pr_info(" Write Function\n");
-wait_queue_flag = 2;
-wake_up_interruptible(&wait_queue_etx);
-```
--> "[ 6254.311361]  Event Came From Write Function : 1"
+# 1. Write
 
-The function "wait_function" is called again
+``` make
+sudo echo "hello world" > /proc/my_value/value
 ```
-static int wait_function(void *unused){
-  while(1) {
+
+or
+
+``` cpp
+write(fd, write_buff, "hello world", 11); 
 ```
--> "[ 6254.311374]  Waiting For Event..."
+
+# 2. Read
+
+``` cpp
+read(fd, read_buff, 256); 
+```
--- device_driver/k09_procfs/main.c
+++ device_driver/k09_procfs/main.c
@@ -2,93 +2,83 @@
 #include <linux/init.h>
 #include <linux/proc_fs.h>
 
-/* Meta Information */
-MODULE_LICENSE("GPL");
-MODULE_AUTHOR("Johannes 4 GNU/Linux");
-MODULE_DESCRIPTION("Module creates a folder and file in procfs and implements read and write callbacks");
-
 /* global variables for procfs folder and file */
 static struct proc_dir_entry *proc_folder;
 static struct proc_dir_entry *proc_file;
 
+char buff_data[255];
+int16_t buff_len = 0;
+
 /**
  * @brief Read data out of the buffer
  */
-static ssize_t my_read(struct file *File, char *user_buffer, size_t count, loff_t *offs) {
-	char text[] = "Hello from a procfs file!\n";
+static ssize_t proc_fs_read(struct file *File, char *user_buffer, size_t count, loff_t *offs) {
 	int to_copy, not_copied, delta;
-
 	/* Get amount of data to copy */
-	to_copy = min(count, sizeof(text));
-
+	to_copy = min(count, sizeof(buff_data));
 	/* Copy data to user */
-	not_copied = copy_to_user(user_buffer, text, to_copy);
-
+	not_copied = copy_to_user(user_buffer, buff_data, to_copy);
 	/* Calculate data */
 	delta = to_copy - not_copied;
-
 	return delta;
 }
-
 /**
  * @brief Write data to buffer
  */
-static ssize_t my_write(struct file *File, const char *user_buffer, size_t count, loff_t *offs) {
-	char text[255];
+static ssize_t proc_fs_write(struct file *File, const char *user_buffer, size_t count, loff_t *offs) {
 	int to_copy, not_copied, delta;
-
 	/* Clear text */
-	memset(text, 0, sizeof(text));
-
+	memset(buff_data, 0, sizeof(buff_data));
 	/* Get amount of data to copy */
-	to_copy = min(count, sizeof(text));
-
+	to_copy = min(count, sizeof(buff_data));
 	/* Copy data to user */
-	not_copied = copy_from_user(text, user_buffer, to_copy);
-	printk("procfs_test - You have written %s to me\n", text);
-
+	not_copied = copy_from_user(buff_data, user_buffer, to_copy);
 	/* Calculate data */
 	delta = to_copy - not_copied;
-
 	return delta;
 }
 
-static struct proc_ops fops = {
-	.proc_read = my_read,
-	.proc_write = my_write,
+static const struct proc_ops fops = {
+	.proc_read = proc_fs_read,
+	.proc_write = proc_fs_write,
 };
 
 /**
  * @brief This function is called, when the module is loaded into the kernel
  */
-static int __init my_init(void) {
+static int __init init_func(void) {
 	/* /proc/hello/dummy */
 
-	proc_folder = proc_mkdir("hello", NULL);
+	proc_folder = proc_mkdir("my_value", NULL);
 	if(proc_folder == NULL) {
-		printk("procfs_test - Error creating /proc/hello\n");
+		printk("procfs_test - Error creating /proc/my_value\n");
 		return -ENOMEM;
 	}
 
-	proc_file = proc_create("dummy", 0666, proc_folder, &fops);
+	proc_file = proc_create("value", 0666, proc_folder, &fops);
 	if(proc_file == NULL) {
-		printk("procfs_test - Error creating /proc/hello/dummy\n");
+		printk("procfs_test - Error creating /proc/my_value/value\n");
 		proc_remove(proc_folder);
 		return -ENOMEM;
 	}
 
-	printk("procfs_test - Created /proc/hello/dummy\n");
+	printk("	Info! Inserted a device\n");
 	return 0;
 }
 
 /**
  * @brief This function is called, when the module is removed from the kernel
  */
-static void __exit my_exit(void) {
-	printk("procfs_test - Removing /proc/hello/dummy\n");
+static void __exit exit_func(void) {
+	printk("	Info! Removed the device\n");
 	proc_remove(proc_file);
 	proc_remove(proc_folder);
 }
 
-module_init(my_init);
-module_exit(my_exit);
+module_init(init_func);
+module_exit(exit_func);
+
+/* Meta Information */
+MODULE_LICENSE("GPL");
+MODULE_AUTHOR("knghia");
+MODULE_DESCRIPTION("a simple device");
