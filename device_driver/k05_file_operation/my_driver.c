51d0d4cf17f6535afe84b4bffbc54d299db1a294
knghia <nghia.nk97@gmail.com>
2023-08-03 17:45:51 +0700

    file operation

--- device_driver/k05_file_operation/my_driver.c
+++ device_driver/k05_file_operation/my_driver.c
@@ -1,89 +1,83 @@
-#include <linux/kernel.h>
 #include <linux/init.h>
 #include <linux/module.h>
-#include <linux/kdev_t.h>
+#include <linux/kernel.h>
 #include <linux/fs.h>
-#include <linux/err.h>
-#include <linux/cdev.h>
 #include <linux/device.h>
+#include <linux/uaccess.h>
 
-dev_t dev = MKDEV(11, 0);
-static struct class *my_dev_class;
-static struct cdev my_cdev;
+#define printd(comment)     printk(KERN_ALERT "%s: %s %d\n",comment, __FUNCTION__, __LINE__); 
+#define DEVICE_NAME         "etx_device"
+#define CLASS_NAME          "etx_class"
+#define IRQ_NO              11
 
-// function prototypes
-static int file_open(struct inode *inode, struct file *file);
-static int file_release(struct inode *inode, struct file *file);
-static ssize_t file_read(struct file *filp, char __user *buf, size_t len, loff_t *off);
-static ssize_t file_write(struct file *filp, const char *buf, size_t len, loff_t *off);
-
-static struct file_operations fops = {
-    .owner = THIS_MODULE,
-    .read = file_read,
-    .write = file_write,
-    .open = file_open,
-    .release = file_release,
-};
+// setting up the device
+int major;
+static struct class*  my_class  = NULL;
+static struct device* my_device = NULL;
 
 static int file_open(struct inode *inode, struct file *file){
-    pr_info("drive open function called \r\n");
+    printd("\t info ! ");
     return 0;
 }
 static int file_release(struct inode *inode, struct file *file){
-    pr_info("drive release function call \r\n");
+    printd("\t info ! ");
     return 0;
 }
 static ssize_t file_read(struct file *filp, char __user *buf, size_t len, loff_t *off){
-    pr_info("drive read function call \r\n");
+    printd("\t info ! ");
     return 0;
 }
 static ssize_t file_write(struct file *filp, const char *buf, size_t len, loff_t *off){
-    pr_info("drive write function call \r\n");
+    printd("\t info ! ");
     return len;
 }
-/*
-* module function start
-*/
-static int __init start_func(void){
-    pr_info("starting \r\n");
-    register_chrdev_region(&dev, 1, "my_dev");
-    pr_info("major = %d minor = %d \n",MAJOR(dev), MINOR(dev));
 
-    cdev_init(&my_cdev, &fops);
-    if(cdev_add(&my_cdev,dev,1) < 0){
-        pr_err("Can\'t create the character device \n");
-        class_destroy(my_dev_class);
-        return -1;
+static struct file_operations fops = {
+    .open = file_open,
+    .release = file_release,
+    .read = file_read,
+    .write = file_write
+};
+
+static int __init etx_device_init(void) {
+    major = register_chrdev(0, DEVICE_NAME, &fops);
+
+    if ( major < 0 )
+        printd("\t error ! ");
+    // Register device class
+    my_class = class_create(THIS_MODULE, CLASS_NAME);
+    if (IS_ERR(my_class)) {
+        printd("\t error ! ");
+        goto r_class;
     }
-    if(IS_ERR(my_dev_class = class_create(THIS_MODULE,"my_class"))){
-        pr_err("Can\'t create the struct class \r\n");
-        class_destroy(my_dev_class);
-        return -1;
+    // Register the device driver
+    my_device = device_create(my_class, NULL, MKDEV(major, 0), NULL, DEVICE_NAME);
+    if (IS_ERR(my_device)) {
+        printd("\t error ! ");
+        goto r_device;
     }
-    if(IS_ERR(device_create(my_dev_class, NULL, dev, NULL, "my_dev"))){
-        pr_err("Can\'t create the device \r\n");
-        unregister_chrdev_region(dev, 1);
-        return -1;
-    }
-    pr_info("device driver inserted \r\n");
+
+    printd("\t init ! ");
     return 0;
-}
-/*
-* module function end
-*/
-static void __exit end_func(void){
-    device_destroy(my_dev_class, dev);
-    class_destroy(my_dev_class);
-    cdev_del(&my_cdev);
-    unregister_chrdev_region(dev,1);
-    pr_info("end !...");
+r_class:
+    unregister_chrdev(major, DEVICE_NAME);
+r_device:
+    class_destroy(my_class);
+    unregister_chrdev(major, DEVICE_NAME);
+    return -1;
 }
 
-module_init(start_func);
-module_exit(end_func);
+static void __exit etx_device_exit(void) {
+    device_destroy(my_class, MKDEV(major, 0));              // remove the device
+    class_unregister(my_class);                             // unregister the device class
+    class_destroy(my_class);                                // remove the device class
+    unregister_chrdev(major, DEVICE_NAME);                  // unregister the major number
+    printd("\t exit ! ");
+}
 
-MODULE_AUTHOR("nk");
-MODULE_LICENSE("GLP");
-MODULE_DESCRIPTION("file operations");
-MODULE_VERSION("0:0.5");
+module_init(etx_device_init);
+module_exit(etx_device_exit);
 
+MODULE_AUTHOR("knghia");
+MODULE_DESCRIPTION("a simple device");
+MODULE_LICENSE("GPL");
